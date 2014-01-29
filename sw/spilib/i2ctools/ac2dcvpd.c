8d23b28356cd7cff7fa21243a2cf6ab59bba6809
zvisha <zvisha@gmail.com>
2014-01-29 01:14:34 -0800

    finalzied read ac2dc vpd both in script (using i2cget,i2cdetect) and in c code

---
+++ sw/spilib/i2ctools/ac2dcvpd.c
@@ -0,0 +1,131 @@
+///
+///simple util that reads AC2DC VPD info
+///and prints it out.
+///
+
+
+#include <stdio.h>
+#include <sys/un.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <string.h>
+#include <pthread.h>
+//#include "hammer.h"
+#include <spond_debug.h>
+#include "assert.h"
+#include "squid.h"
+#include "i2c.h"
+#include "ac2dc.h"
+#include "dc2dc.h"
+
+
+using namespace std;
+pthread_mutex_t network_hw_mutex = PTHREAD_MUTEX_INITIALIZER;
+
+
+#define ACTION_READ_BYTE   0
+#define ACTION_READ_WORD   1
+#define ACTION_WRITE_BYTE  2
+#define ACTION_WRITE_WORD  3
+
+
+int usage(char * app ,int exCode ,const char * errMsg = NULL)
+{
+    if (NULL != errMsg )
+    {
+        fprintf (stderr,"==================\n%s\n\n==================\n",errMsg);
+    }
+    
+    printf ("Usage: %s [-q] [-a] [-p] [-m] [-r] [-s]\n\n" , app);    
+
+    printf ("       -q : quiet mode, values only w/o headers\n"); 
+    printf ("       -a : print all VPD params as one string\n"); 
+    printf ("       -p : print part number\n"); 
+    printf ("       -m : print model ID\n"); 
+    printf ("       -r : print revision number\n"); 
+    printf ("       -s : print serial number\n"); 
+
+    if (0 == exCode) // exCode ==0 means - just print usage and get back to app for business. other value - exit with it.
+    {
+        return 0;
+    }
+    else 
+    {
+        exit(exCode);
+    }
+}
+
+int main(int argc, char *argv[])
+{
+     int rc = 0;
+
+	bool callUsage = false;
+	bool badParm = false;
+	bool quiet = false;
+	bool print_all = false;
+	bool print_pnr = false;
+	bool print_mod = false;
+	bool print_rev = false;
+	bool print_ser = false;
+
+	const char * h_all = "AC2DC VPD: ";
+	const char * h_pnr = "AC2DC PNR: ";
+	const char * h_mod = "AC2DC MOD: ";
+	const char * h_rev = "AC2DC REV: ";
+	const char * h_ser = "AC2DC SER: ";
+	
+	for (int i = 1 ; i < argc ; i++){
+		if ( 0 == strcmp(argv[i],"-h"))
+			callUsage = true;
+		else if ( 0 == strcmp(argv[i],"-q"))
+			quiet = true;
+		else if ( 0 == strcmp(argv[i],"-a"))
+			print_all = true;
+		else if ( 0 == strcmp(argv[i],"-p"))
+			print_pnr = true;
+		else if ( 0 == strcmp(argv[i],"-m"))
+			print_mod = true;
+		else if ( 0 == strcmp(argv[i],"-r"))
+			print_rev = true;
+		else if ( 0 == strcmp(argv[i],"-s"))
+			print_ser = true;
+		else
+			badParm = true;
+
+	}
+
+	// if no print spec was given (all are false, then set all sub fields (except for all)
+	if ( false == (print_all || print_pnr || print_mod || print_rev || print_ser) ){
+		print_pnr = true;
+		print_mod = true;
+		print_rev = true;
+		print_ser = true;
+	}
+
+	if(badParm)
+	{
+		usage(argv[0],1,"Bad arguments");
+	}	
+
+	if (callUsage)
+		return usage(argv[0] , 0);
+
+    	i2c_init();
+
+    	ac2dc_vpd_info_t vpd = {}; // allocte, and initializero
+		
+	ac2dc_get_vpd(&vpd);
+
+	if (print_all)
+		printf("%s%s%s%s\n",quiet?"":h_all,vpd.pnr,vpd.model,vpd.serial);
+	if (print_pnr)
+		printf("%s%s\n",quiet?"":h_pnr,vpd.pnr);
+	if (print_mod)
+		printf("%s%s\n",quiet?"":h_mod,vpd.model);
+	if (print_rev)
+		printf("%s%s\n",quiet?"":h_rev,vpd.revision);
+	if (print_ser)
+		printf("%s%s\n",quiet?"":h_ser,vpd.serial);
+
+	return 0;
+}
