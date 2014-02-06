a00a4336e7565d0a09ff13c50e0f78561c378e4b
zvisha <zvisha@gmail.com>
2014-02-06 06:47:35 -0800

    bugfix

--- sw/scripts/gen-spond-host.sh
+++ sw/scripts/gen-spond-host.sh
@@ -8,4 +8,4 @@ START=28
 
 # mng board eeprom range start for box serial name
 END=39
-echo "${HOST_PREF=}`./read-mng-eeprom-stripped.sh 28 39`"
+echo "${HOST_PREF=}`read-mng-eeprom-stripped.sh 28 39`"
