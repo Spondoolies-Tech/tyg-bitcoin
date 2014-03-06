6f649e285bf82f1a9489db5569f2a84c555bb36a
zvisha <zvisha@gmail.com>
2014-03-06 06:11:00 -0800

    missing files added

---
+++ sw/spilib/miner_gate/ac2dc_scaling.h
@@ -0,0 +1,14 @@
+#ifndef _____AC2DCSCALINGME__32R_H____
+#define _____AC2DCSCALINGME__32R_H____
+#include "defines.h"
+
+
+/*
+  HAMMER *find_asic_to_increase_speed(int l);
+  void asic_upscale(HAMMER *a);
+  */
+
+
+
+#endif
+
---
+++ sw/spilib/miner_gate/leakage_discovery.h
@@ -0,0 +1,11 @@
+#ifndef _____DCAJFAKFSFAKSFHASKLIXT__45R_H____
+#define _____DCAJFAKFSFAKSFHASKLIXT__45R_H____
+
+#include "defines.h"
+
+void do_leakage_discovery();
+
+
+
+#endif
+
