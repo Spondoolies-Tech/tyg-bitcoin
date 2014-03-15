1b05250c5b3ea11367aa19f8e281a9698b696c2e
Vladik Goytin <goytin@gmail.com>
2014-03-15 21:32:11 +0200

    rrd: take care of /var/www/rrd directory
    For various reasons /var/www/rrd should be symlinked to /tmp/rrd.

--- sw/vladik/tools/customize-rootfs.sh
+++ sw/vladik/tools/customize-rootfs.sh
@@ -189,7 +189,11 @@ sw_upgrade()
 cron()
 {
 	cp -a ${CUR_DIR}/../add-ons/S47cron etc/init.d
+	cp -a ${CUR_DIR}/../add-ons/S13misc etc/init.d
 	cp ${CUR_DIR}/../../../../minepeon/etc/cron.d/5min/RECORDHashrate etc/cron.d
+
+	# Should be deleted as it is symlinked to /tmp/rrd in runtime.
+	rm -rf var/www/rrd
 
 	# Run every minute
 	echo '* * * * * /usr/bin/php /etc/cron.d/RECORDHashrate' > etc/cron.d/crontabs/root
