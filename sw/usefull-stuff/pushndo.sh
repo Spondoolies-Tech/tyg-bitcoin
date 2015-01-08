986fc11853e24038fb707c230b66b1adfb77c977
remo <rimonl@gmail.com>
2015-01-08 16:35:52 +0200

    adding params passing to pushndo

--- sw/usefull-stuff/pushndo.sh
+++ sw/usefull-stuff/pushndo.sh
@@ -6,7 +6,7 @@ ping -c 1 -W 1 $IP > /dev/null 2>&1
 if [ $? -eq 0 ] ; then
 	PUSH=`sshpass -p root scp -o StrictHostKeyChecking=no $SCR root@${IP}:/usr/local/bin `
 	sshpass -p root ssh -o StrictHostKeyChecking=no root@${IP} chmod +x /usr/local/bin/$SCR 
-	DO=`sshpass -p root ssh -o StrictHostKeyChecking=no root@${IP} /usr/local/bin/${SCR} `
+	DO=`sshpass -p root ssh -o StrictHostKeyChecking=no root@${IP} /usr/local/bin/${SCR} ${@:3} `
 	if [ $? -eq 0 ] ; then 
 		echo ${IP},${FW},${OP}
 	else
