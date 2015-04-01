889bcf7a9edd814ab87356a0ac58f20860c38c77
remo <rimonl@gmail.com>
2015-04-01 15:25:32 +0300

    some some some omre

--- sw/usefull-stuff/findwallet.sh
+++ sw/usefull-stuff/findwallet.sh
@@ -1,11 +1,3 @@
 #!/bin/bash
 
-#WALLET=1M72Sfpbz1BPpXFHz9m3CdqATR44Jvaydd
-SCRIPT=`dirname $0`/findminertags.sh
-
-if [ ! -e ${SCRIPT} ] ; then
-	echo Script ${SCRIPT} not found
-	exit 1
-fi
-
-${SCRIPT} $1 $2 /etc/cgminer.conf
+findminertags.sh $1 $2 /etc/cgminer.conf
