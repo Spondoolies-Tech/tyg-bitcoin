79bb5607f359b406e351293f8c9cf1c09b84e4bd
remo <rimonl@gmail.com>
2015-04-01 15:17:27 +0300

    some more cleans up

--- sw/usefull-stuff/findminertags.sh
+++ sw/usefull-stuff/findminertags.sh
@@ -1,18 +1,15 @@
 #!/bin/bash
 . $(dirname $0)/constants
 
-#WALLET=1M72Sfpbz1BPpXFHz9m3CdqATR44Jvaydd
 
 
 
 if [ $# -lt 3 ] ; then
-	echo "$0 <miner> <searchtag> <miner_file_to_grep>"
+	echo "Usage: $(fname $0) <miner> <searchtag> <miner_file_to_grep>"
 fi
 LOOKATTRG=$3
 TAG=$2
 MINER=$1
-
-#echo $@
 
 ping -c 1 -W 1 ${MINER} > /dev/null 2>&1
 
